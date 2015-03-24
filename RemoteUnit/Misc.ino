df53dc2846f868317d92f12858db56bd9b88c4a8
Jiří Macháček <jiri.machacek87@gmail.com>
2015-03-24 10:55:17 +0100

    Update Misc.ino

--- RemoteUnit/Misc.ino
+++ RemoteUnit/Misc.ino
@@ -27,3 +27,32 @@ int freeRam() {
 	return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
 }
 
+int millisRollover() {
+  // get the current millis() value for how long the microcontroller has been running
+  //
+  // To avoid any possiblity of missing the rollover, we use a boolean toggle that gets flipped
+  //   off any time during the first half of the total millis period and
+  //   then on during the second half of the total millis period.
+  // This would work even if the function were only run once every 4.5 hours, though typically,
+  //   the function should be called as frequently as possible to capture the actual moment of rollover.
+  // The rollover counter is good for over 35 years of runtime. --Rob Faludi http://rob.faludi.com
+  //
+  static int numRollovers=0; // variable that permanently holds the number of rollovers since startup
+  static boolean readyToRoll = false; // tracks whether we've made it halfway to rollover
+  unsigned long now = millis(); // the time right now
+  unsigned long halfwayMillis = 2147483647; // this is halfway to the max millis value (17179868 for earlier versions of Arduino)
+
+  if (now > halfwayMillis) { // as long as the value is greater than halfway to the max
+    readyToRoll = true; // you are ready to roll over
+  }
+
+  if (readyToRoll == true && now < halfwayMillis) {
+    // if we've previously made it to halfway
+    // and the current millis() value is now _less_ than the halfway mark
+    // then we have rolled over
+    numRollovers++; // add one to the count the number of rollovers
+    readyToRoll = false; // we're no longer past halfway
+  } 
+  return numRollovers;
+}
+
