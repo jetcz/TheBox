49c0552f5be853fe09d4cb97a36a52d26ddd0510
jetcz <jiri.machacek87@gmail.com>
2015-09-16 18:54:18 +0200

    add private data

---
+++ MainUnit/PrivateData.h
@@ -0,0 +1,9 @@
+/// <summary>
+/// Structure for keeping private data
+/// </summary>
+struct PrivateData
+{
+	String MainDSAPIKey = "xxx";
+	String RemoteUnitAPIKey = "xxx";
+	String SystemAPIKey = "xxx";
+}; typedef struct PrivateData PrivateData;
